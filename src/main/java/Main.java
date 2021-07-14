7c908d1eb30a0dcb0fcc4ba7d9e4b2ef80ecb723
HakobyanRob <hakobyan.rob@gmail.com>
2021-07-14 21:04:49 +0400

    Adding basic functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,26 @@
 import home.project.SelfBalancingBinaryTree;
 
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         SelfBalancingBinaryTree selfBalancingBinaryTree = new SelfBalancingBinaryTree();
 
-        selfBalancingBinaryTree.add(1);
-        selfBalancingBinaryTree.add(12);
-        selfBalancingBinaryTree.add(45);
-        selfBalancingBinaryTree.add(5);
-        selfBalancingBinaryTree.add(4);
-        selfBalancingBinaryTree.add(9);
-        selfBalancingBinaryTree.add(3);
-        selfBalancingBinaryTree.add(12);
-        selfBalancingBinaryTree.add(56);
+        selfBalancingBinaryTree.addIteratively(1);
+        selfBalancingBinaryTree.addIteratively(12);
+        selfBalancingBinaryTree.addIteratively(45);
+        selfBalancingBinaryTree.addIteratively(5);
+        selfBalancingBinaryTree.addIteratively(4);
+        selfBalancingBinaryTree.addIteratively(9);
+        selfBalancingBinaryTree.addIteratively(3);
+        selfBalancingBinaryTree.addIteratively(12);
+        selfBalancingBinaryTree.addIteratively(56);
 
         System.out.println(selfBalancingBinaryTree.getSize());
 
-        selfBalancingBinaryTree.find(3);
+        selfBalancingBinaryTree.findIteratively(3);
+
+        Set<Integer> allNodes = selfBalancingBinaryTree.getAllNodes();
+        System.out.println(allNodes);
     }
 }
--- src/main/java/home/project/SelfBalancingBinaryTree.java
+++ src/main/java/home/project/SelfBalancingBinaryTree.java
@@ -1,5 +1,9 @@
 package home.project;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class SelfBalancingBinaryTree {
 
     private Node root;
@@ -9,12 +13,8 @@ public class SelfBalancingBinaryTree {
         return size;
     }
 
-    public void add(int value) {
+/*    public void add(int value) {
         root = addRecursive(root, value);
-    }
-
-    public Node find(int value) {
-        return findRecursive(root, value);
     }
 
     private Node addRecursive(Node current, int value) {
@@ -32,7 +32,53 @@ public class SelfBalancingBinaryTree {
         }
 
         return current;
+    }*/
+
+    public Node find(int value) {
+        return findRecursive(root, value);
     }
+
+    public Node addIteratively(int value) {
+        Node current = root;
+        Node temp = null;
+
+        while (current != null) {
+            temp = current;
+            if (current.value < value) {
+                current = current.right;
+            } else if (current.value > value) {
+                current = current.left;
+            } else {
+                return temp;
+            }
+        }
+
+        if (temp == null) {
+            root = new Node(value);
+        } else if (temp.value > value) {
+            temp.left = new Node(value);
+        } else {
+            temp.right = new Node(value);
+        }
+        size++;
+        return temp;
+    }
+
+    public Node findIteratively(int value) {
+        Node current = root;
+
+        while (current != null) {
+            if (current.value == value) {
+                return current;
+            } else if (current.value > value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return null;
+    }
+
 
     private Node findRecursive(Node current, int value) {
         if (current == null) {
@@ -44,5 +90,21 @@ public class SelfBalancingBinaryTree {
         } else {
             return current;
         }
+    }
+
+    public Set<Integer> getAllNodes() {
+        return getAllNodes(root);
+    }
+
+    public Set<Integer> getAllNodes(Node current) {
+        Set<Integer> allNodes = new LinkedHashSet<>();
+
+        if (current != null) {
+
+            allNodes.addAll(getAllNodes(current.left));
+            allNodes.add(current.value);
+            allNodes.addAll(getAllNodes(current.right));
+        }
+        return allNodes;
     }
 }
